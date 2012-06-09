ed9a77b5ed8e34f41f059da1e36ee6a093985a0a
Jonathan Griffin <jgriffin@mozilla.com>
2012-06-08 17:50:42 -0700

    Update builder1 jenkins configs

--- jenkins/builder1/update.sh
+++ jenkins/builder1/update.sh
@@ -2,8 +2,19 @@
 
 PWD=`dirname $0`
 
-scp jgriffin@builder1.ateam.phx1.mozilla.com:/data/jenkins/jobs/build-emulator-arm/config.xml $PWD/build-emulator-arm/config.xml
-scp jgriffin@builder1.ateam.phx1.mozilla.com:/data/jenkins/jobs/build-emulator-x86/config.xml $PWD/build-emulator-x86/config.xml
-scp jgriffin@builder1.ateam.phx1.mozilla.com:/data/jenkins/jobs/build-nexus-s/config.xml $PWD/build-nexus-s/config.xml
-scp jgriffin@builder1.ateam.phx1.mozilla.com:/data/jenkins/jobs/build-sgs2/config.xml $PWD/build-sgs2/config.xml
+REMOTE_DIR="/data/jenkins/jobs"
+
+#scp jgriffin@builder1.ateam.phx1.mozilla.com:/data/jenkins/jobs/build-emulator-arm/config.xml $PWD/build-emulator-arm/config.xml
+#scp jgriffin@builder1.ateam.phx1.mozilla.com:/data/jenkins/jobs/build-emulator-x86/config.xml $PWD/build-emulator-x86/config.xml
+#scp jgriffin@builder1.ateam.phx1.mozilla.com:/data/jenkins/jobs/build-nexus-s/config.xml $PWD/build-nexus-s/config.xml
+#scp jgriffin@builder1.ateam.phx1.mozilla.com:/data/jenkins/jobs/build-sgs2/config.xml $PWD/build-sgs2/config.xml
+
+for i in `ssh builder1.ateam.phx1.mozilla.com ls $REMOTE_DIR`
+do
+    echo $i
+    if [ ! -d $i ]; then
+      mkdir $i
+    fi
+    scp builder1.ateam.phx1.mozilla.com:$REMOTE_DIR/$i/config.xml $PWD/$i/config.xml
+done
 
