cb3152ea8f3683c9e18e71e6f8f8b920880a8d29
Jonathan Griffin <jgriffin@mozilla.com>
2012-10-16 09:58:27 -0700

    Bug 801972 - Get the buildid from application.ini

--- updates/nightly/makeupdate.sh
+++ updates/nightly/makeupdate.sh
@@ -1,12 +1,16 @@
 #exit 0
 
 FILE=`ls -rt *.mar | tail -1`
-VERSION="18.0a1"
+APPVERSION="18.0a1"
+VERSION="18.0a2"
 SHA512=$(shasum -a 512 $FILE | sed 's/ .*//')
-BUILD_ID=$(echo $FILE | sed 's/b2g_update_//' | sed 's/\.mar//' | sed 's/[-_]//g')
+DATESTAMP=$(echo $FILE | sed 's/b2g_update_//' | sed 's/\.mar//')
+INI="application_${DATESTAMP}.ini"
+BUILD_ID=$(grep BuildID= $INI | sed 's/BuildID=//')
 SIZE=$(stat -c "%s" $FILE)
 
 cat update-template.xml \
+  | sed "s/\\\$APPVERSION\\\$/$APPVERSION/g" \
   | sed "s/\\\$VERSION\\\$/$VERSION/g" \
   | sed "s/\\\$HASH\\\$/$SHA512/g" \
   | sed "s/\\\$SIZE\\\$/$SIZE/g" \
